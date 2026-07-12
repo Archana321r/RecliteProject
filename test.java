25381f3fc9b36bf9486cfe38723d0997aac3c36e
Archana321r <archanarathod729@gmail.com>
2026-07-12 19:54:24 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,10 @@
+class ATM {
+
+public static void main(string[] args)
+{
+system.out.printIn("Hello, Myself Archana ");
+System.out.printIn("An Automatic Teller Machine                                                                                                                                                                                                                                                                                                                                                                                                                                                                         ");
+
+}
+
+}
